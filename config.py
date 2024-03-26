24aa326985efa90662fbbc5f432cc7bf3569ba07
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-26 13:47:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class db:
-  DB_URL = ""
+  DB_URL = "sjsusbsnsns"
   
 class vars:
-  API_ID = 
-  API_HASH = ""
-  TOKEN = ""  
+  API_ID = 2873737
+  API_HASH = "jdjuiejedndbbr"
+  TOKEN = "6939383838:jeiehdnsjsjsjj"  
   BOT_USERNAME = ""     
   START_MSG = """
   **Hello {}** it's Unchained File Bot,\n**Made by @UnchainedCodes**
@@ -13,11 +13,11 @@ class vars:
   START_PIC = "https://telegra.ph/file/aab6cd1be28c75bea480d.jpg"
   CHANNEL = "UnchainedHanime"
   CHANNEL_NAME = "Unchained Hanime"
-  DB_CHANNEL = 
+  DB_CHANNEL = -3838393883 
   DEVS = [282829299, 39939393]
   OWNER_ID = 6823076870
   
 class keys:
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
   s_header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-  shortner_api = ""
+  shortner_api = "eijsjsjsjsnsnshshs"
