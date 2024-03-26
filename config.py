6cfce53c77cb9eda68bcb32f46a42744d361861d
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-26 12:01:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+class db:
+  DB_URL = ""
+  
+class vars:
+  API_ID = 
+  API_HASH = ""
+  TOKEN = ""  
+  BOT_USERNAME = ""     
+  START_MSG = """
+  **Hello {}** it's Unchained File Bot,\n**Made by @UnchainedCodes**
+  """
+  FILE_CAPTION = "Here is your file\nJoin: @UnchainedHanime"
+  START_PIC = "https://telegra.ph/file/aab6cd1be28c75bea480d.jpg"
+  CHANNEL = "UnchainedHanime"
+  CHANNEL_NAME = "Unchained Hanime"
+  DB_CHANNEL = 
+  DEVS = [282829299, 39939393]
+  OWNER_ID = 6823076870
+  
+class keys:
+  headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
+  s_header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+  shortner_api = ""
