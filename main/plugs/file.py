5c1f0c267b64f994bfcab44e5f63f34f047e2c2d
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-26 16:09:49 +0530

    new funcs.py

--- main/plugs/file.py
+++ main/plugs/file.py
@@ -5,54 +5,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.enums import ParseMode
 from main import DEVS, CHANNEL, BOT_USERNAME, encode, gplinks 
+from main.funcs import get_message_id 
 from mongodb.users import Users   
-
-async def get_message_id(client, message):
-    if message.forward_from_chat:
-        if message.forward_from_chat.id == client.db_channel.id:
-            return message.forward_from_message_id
-        else:
-            return 0
-    elif message.forward_sender_name:
-        return 0
-    elif message.text:
-        pattern = r"https://t.me/(?:c/)?(.*)/(\d+)"
-        matches = re.match(pattern,message.text)
-        if not matches:
-            return 0
-        channel_id = matches.group(1)
-        msg_id = int(matches.group(2))
-        if channel_id.isdigit():
-            if f"-100{channel_id}" == str(client.db_channel.id):
-                return msg_id
-        else:
-            if channel_id == client.db_channel.username:
-                return msg_id
-    else:
-        return 0
-
-async def get_messages(client, message_ids):
-    messages = []
-    total_messages = 0
-    while total_messages != len(message_ids):
-        temp_ids = message_ids[total_messages:total_messages+200]
-        try:
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temp_ids
-            )
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temp_ids
-            )
-        except Exception as e:
-            print(f"Error fetching messages: {e}")
-        total_messages += len(temp_ids)
-        messages.extend(msgs)
-    return messages
-
 
 @Client.on_message(filters.private & filters.all & filters.user(DEVS)) #& ~filters.regex('^/'))
 async def channel_post(client: Client, message: Message):
