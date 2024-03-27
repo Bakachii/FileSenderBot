1ec8dddf31a8b4419913e2a32553be6ef25925c6
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-27 13:43:29 +0530

    Update file.py

--- main/plugs/file.py
+++ main/plugs/file.py
@@ -57,11 +57,12 @@ async def link_generator(client: Client, message: Message):
             channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
-        msg_id = await get_message_id(client, channel_message)
+        get_id = await get_message_id(client, channel_message)    
+        msg = int(get_id)
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            await channel_message.reply("This Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
             continue
     
     base64_string = await encode(f"{BOT_USERNAME}-{msg_id * abs(client.db_channel.id)}")
@@ -76,7 +77,6 @@ async def link_generator(client: Client, message: Message):
         ] 
     )
     await message.edit_text(f"• Encoded links\n﹂Link:\n`{link}`\n﹂GpLink:\n`{gplink}`", reply_markup=reply_markup)
-    isTask = False   # Set isTask to False at the end of the command
 
 
 @Client.on_message(filters.private & filters.user(DEVS) & ~filters.regex(r'^/'))
