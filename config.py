93cab25771a5dece5cc6ac70d2c7cb6501a7e16a
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-27 20:41:48 +0530

    Added Auto Delete Feature

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class vars:
   DB_CHANNEL = -3838393883 
   DEVS = [282829299, 39939393]
   OWNER_ID = 6823076870
-  
+  SECONDS = 360
 class keys:
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
   s_header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
