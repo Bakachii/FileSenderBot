077943230c05f48825338d9dba31d38fe79f97b7
Tanjirou Kamado <163954611+Bakachii@users.noreply.github.com>
2024-03-27 14:00:42 +0530

    Update file.py

--- main/plugs/file.py
+++ main/plugs/file.py
@@ -73,7 +73,7 @@ async def link_generator(client: Client, message: Message):
             ]
         ] 
     )
-    await message.edit_text(f"• Encoded links\n﹂Link:\n`{link}`\n﹂GpLink:\n`{gplink}`", reply_markup=reply_markup)
+    await message.reply_text(f"• Encoded links\n﹂Link:\n`{link}`\n﹂GpLink:\n`{gplink}`", reply_markup=reply_markup)
 
 
 @Client.on_message(filters.private & filters.user(DEVS) & ~filters.regex(r'^/'))
